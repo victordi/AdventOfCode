1148d7e2e3e2b31b1bb678bc3c05dc171515572b
Victor Diaconu <victor.diaconu@ing.com>
2022-12-20 10:12:25 +0200

    Day20

--- src/main/kotlin/advent2022/day20/main.kt
+++ src/main/kotlin/advent2022/day20/main.kt
@@ -1,15 +1,23 @@
 package advent2022.day20
 
+val input = Input.readInput().map { it.toInt() }
+val SIZE = input.size
 
 fun main() {
-    println(first())
-    println(second())
+    println(solve())
+    println(solve(811589153L, 10))
 }
 
-fun first(): Int = run {
-    0
+fun MutableList<Pair<Int, Long>>.move(i: Int) {
+    val (idx, value) = withIndex().first { it.value.first == i }
+    val newIdx = (idx.toLong() + value.second).mod(SIZE.toLong() - 1L).toInt()
+    removeAt(idx)
+    add(newIdx, value)
 }
 
-fun second(): Int = run {
-    0
+fun solve(key: Long = 1L, times: Int = 1): Long = run {
+    val indexed = input.withIndex().map { it.index to it.value.toLong() * key }.toMutableList()
+    repeat(times) { (0 until SIZE).forEach { indexed.move(it) } }
+    val idxZero = indexed.indexOfFirst { it.second == 0L }
+    indexed[(idxZero + 1000) % SIZE].second + indexed[(idxZero + 2000) % SIZE].second + indexed[(idxZero + 3000) % SIZE].second
 }
