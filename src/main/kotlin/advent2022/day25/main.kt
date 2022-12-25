70f44ffe85b7f580949b5603ddfe2465b27922db
Victor Diaconu <victor.diaconu@ing.com>
2022-12-25 08:54:15 +0200

    Final day

--- src/main/kotlin/advent2022/day25/main.kt
+++ src/main/kotlin/advent2022/day25/main.kt
@@ -1,10 +1,40 @@
 package advent2022.day25
 
+import Input
 
 fun main() {
-    println(first())
+    val sum = first()
+    println(sum)
+    println(sum.getSNAFU())
 }
 
-fun first(): Int = run {
-    0
+fun first(): Long = Input
+    .readInput()
+    .map { it.reversed() }
+    .map {
+        var pow5 = 1L
+        var result = 0L
+        it.forEach {
+            if (it.isDigit()) result += it.digitToInt() * pow5
+            else if (it == '-') result -= pow5
+            else if (it == '=') result -= 2 * pow5
+            pow5 *= 5
+        }
+        result
+    }
+    .sum()
+
+fun Long.getSNAFU(): String {
+    var digits = ""
+    var number = this
+    while (number > 0) {
+        var carry = 0
+        when (val rem = (number % 5).toInt()) {
+            0, 1, 2 -> digits += rem
+            3 -> digits += '='.also { carry = 1 }
+            4 -> digits += '-'.also { carry = 1 }
+        }
+        number = number / 5 + carry
+    }
+    return digits.reversed()
 }
