b7c68f5c94bfefb1f3dd770bad2ec8ade83ee6c9
Victor Diaconu <victor.diaconu@ing.com>
2023-12-01 17:50:01 +0200

    Day1 ... forgot it's December and late start

--- src/main/kotlin/advent2023/day01/main.kt
+++ src/main/kotlin/advent2023/day01/main.kt
@@ -1,16 +1,34 @@
 package advent2023.day01
 
 import Input.withInput
+import arrow.core.foldLeft
 
 fun main() {
   println(first())
   println(second())
 }
 
+val numbers = mapOf(
+  "one" to "o1e", "two" to "t2o", "three" to "t3e", "four" to "f4fr",
+  "five" to "f5e", "six" to "s6x", "seven" to "s7n", "eight" to "e8t", "nine" to "n9e"
+)
+
 fun first(): Int = withInput { input ->
-  0
+  input.toList().sumOf {
+    val digits = it.filter { it.isDigit() }
+    digits.first().digitToInt() * 10 + digits.last().digitToInt()
+  }
 }
 
 fun second(): Int = withInput { input ->
-  0
+  input
+    .toList()
+    .map {
+      numbers.foldLeft(it) { acc, (key, value) ->
+        acc.replace(key, value)
+      }
+    }.sumOf {
+      val digits = it.filter { it.isDigit() }
+      digits.first().digitToInt() * 10 + digits.last().digitToInt()
+    }
 }
