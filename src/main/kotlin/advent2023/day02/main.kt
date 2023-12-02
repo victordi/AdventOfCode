99cb9b06c109b88b4a011a25f32f39ea2d82df6f
Victor Diaconu <victor.diaconu@ing.com>
2023-12-02 12:34:33 +0200

    Day 2

--- src/main/kotlin/advent2023/day02/main.kt
+++ src/main/kotlin/advent2023/day02/main.kt
@@ -1,6 +1,7 @@
 package advent2023.day02
 
 import Input.withInput
+import splitTwo
 
 fun main() {
   println(first())
@@ -8,9 +9,31 @@ fun main() {
 }
 
 fun first(): Int = withInput { input ->
-  0
+  input
+    .toList()
+    .mapIndexed { index, game ->
+      val (maxRed, maxBlue, maxGreen) = game.maxColoredCubes()
+      if (maxRed <= 12 && maxGreen <= 13 && maxBlue <= 14) index + 1 else 0
+    }
+    .sum()
 }
 
 fun second(): Int = withInput { input ->
-  0
+  input
+    .toList()
+    .mapIndexed { _, game ->
+      val (maxRed, maxBlue, maxGreen) = game.maxColoredCubes()
+      maxRed * maxBlue * maxGreen
+    }
+    .sum()
 }
+
+fun String.maxColoredCubes(): Triple<Int, Int, Int> {
+  val subGames = this.split(": ")[1].replace(";", ",").split(",").map { it.trim().splitTwo(" ") }
+  val maxRed = subGames.maxCubes("red")
+  val maxBlue = subGames.maxCubes("blue")
+  val maxGreen = subGames.maxCubes("green")
+  return Triple(maxRed, maxBlue, maxGreen)
+}
+
+fun List<Pair<String, String>>.maxCubes(color: String): Int = filter { it.second == color }.maxOf { it.first.toInt() }
