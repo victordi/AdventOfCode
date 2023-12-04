8e24ffb0a3c6e3c279ec74a7eb00976676e98b28
Victor Diaconu <victor.diaconu@ing.com>
2023-12-04 07:41:03 +0200

    Day4

--- src/main/kotlin/advent2023/day04/main.kt
+++ src/main/kotlin/advent2023/day04/main.kt
@@ -1,16 +1,42 @@
 package advent2023.day04
 
 import Input.withInput
+import splitTwo
+import kotlin.math.pow
 
 fun main() {
   println(first())
   println(second())
 }
 
-fun first(): Int = withInput { input ->
-  0
+fun first(): Long = withInput { input ->
+  var result = 0L
+  input.toList().forEach {
+    result += it.scratchScore()
+  }
+  result
 }
 
 fun second(): Int = withInput { input ->
-  0
+  val counts = mutableMapOf<Int, Int>()
+  input.toList().forEachIndexed { index, s ->
+    counts[index] = 1 + (counts[index] ?: 0)
+    (index + 1..index + s.winningNumbers()).forEach { i ->
+      counts[i] = (counts[i] ?: 0) + counts[index]!!
+    }
+  }
+  counts.values.sum()
+}
+
+fun String.scratchScore(): Int {
+  val wins = winningNumbers()
+  return if (wins == 0) 0
+  else 2.toDouble().pow(wins - 1).toInt()
+}
+
+fun String.winningNumbers(): Int {
+  val (win, own) = split(": ")[1].splitTwo(" | ")
+  val winNrs = win.split(" ").map { it.trim().toIntOrNull() ?: -3 }
+  val ownNrs = own.split(" ").map { it.trim().toIntOrNull() ?: -2 }
+  return ownNrs.count { winNrs.contains(it) }
 }
