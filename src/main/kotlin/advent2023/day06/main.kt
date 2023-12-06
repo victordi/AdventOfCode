b80bcdf7e73e9305d1c0167d9ef8d0ad90e1069c
Victor Diaconu <victor.diaconu@ing.com>
2023-12-06 07:28:36 +0200

    Day6

--- src/main/kotlin/advent2023/day06/main.kt
+++ src/main/kotlin/advent2023/day06/main.kt
@@ -8,9 +8,21 @@ fun main() {
 }
 
 fun first(): Int = withInput { input ->
-  0
+  val list = input.toList()
+  val times = list.first().split(":")[1].trim().split(" ").filter { it.isNotEmpty() }.map { it.trim().toInt() }
+  val distances = list[1].split(":")[1].trim().split(" ").filter { it.isNotEmpty() }.map { it.trim().toInt() }
+  val games = times.zip(distances)
+  games.map { (time, distance) ->
+    (1..time).count { it * (time - it) > distance }
+  }.fold(1) { acc, nr -> acc * nr}
 }
 
-fun second(): Int = withInput { input ->
-  0
+fun second(): Long = withInput { input ->
+  val list = input.toList()
+  val time = list[0].split(":")[1].filter { it.isDigit() }.toLong()
+  val distance = list[1].split(":")[1].filter { it.isDigit() }.toLong()
+
+  val start = (1..time).first { it * (time - it) > distance }
+  val stop = (time downTo 1).first { it * (time - it) > distance }
+  stop - start + 1
 }
