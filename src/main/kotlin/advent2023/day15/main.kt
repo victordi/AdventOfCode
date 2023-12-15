c67bad11050bb707a6bec414a09445a303ace2a4
Victor Diaconu <victor.diaconu@ing.com>
2023-12-15 09:37:24 +0200

    Day15

--- src/main/kotlin/advent2023/day15/main.kt
+++ src/main/kotlin/advent2023/day15/main.kt
@@ -1,16 +1,60 @@
 package advent2023.day15
 
-import Input.withInput
+import Input.readInput
+import advent2023.day15.Sign.Equals
+import advent2023.day15.Sign.Minus
 
 fun main() {
   println(first())
   println(second())
 }
 
-fun first(): Int = withInput { input ->
-  0
+val words = readInput().first().split(",")
+
+fun first(): Int = words.sumOf {it.getHash() }
+
+fun String.getHash(): Int = fold(0) { acc, c -> ((acc + c.code) * 17 ) % 256 }
+
+enum class Sign {
+  Equals, Minus
 }
 
-fun second(): Int = withInput { input ->
-  0
+data class Entry(val label: String, val sign: Sign, val lens: Int) {
+  companion object {
+    fun from(value: String): Entry =
+      if (value.contains("=")) {
+        val (s, i) = value.split("=")
+        Entry(s, Equals, i.toInt())
+      } else if (value.contains("-")) {
+        val (s, _) = value.split("-")
+        Entry(s, Minus, 0)
+      } else throw IllegalArgumentException("Invalid entry no = or -")
+  }
+}
+
+fun second(): Int = run {
+  val boxes = (0..255).map { mutableListOf<Entry>() }
+  words.map { Entry.from(it) }.forEach { (label, sign, lens) ->
+    val hash = label.getHash()
+    when(sign) {
+      Equals -> {
+        val inBox = boxes[hash].any { it.label == label }
+        if (inBox) {
+          boxes[hash].replaceAll {
+            if (it.label == label) it.copy(lens = lens)
+            else it
+          }
+        } else boxes[hash].add(Entry(label, sign, lens))
+      }
+      Minus -> {
+        boxes[hash].removeIf { it.label == label }
+      }
+    }
+  }
+
+  boxes.foldIndexed(0) { boxNr, acc, entries ->
+    acc + entries.foldIndexed(0) { entryNr, acc2, entry ->
+      acc2 + (1 + boxNr) * (1 + entryNr) * entry.lens
+    }
+  }
 }
