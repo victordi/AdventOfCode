e9302c0ccf40402d5cf6fe97e10362765ef212fa
Victor Diaconu <victor.diaconu@ing.com>
2023-12-18 08:17:09 +0200

    Day18

--- src/main/kotlin/advent2023/day18/main.kt
+++ src/main/kotlin/advent2023/day18/main.kt
@@ -1,16 +1,64 @@
 package advent2023.day18
 
-import Input.withInput
+import Direction
+import Direction.East
+import Direction.North
+import Direction.South
+import Direction.West
+import Input.readInput
 
 fun main() {
   println(first())
   println(second())
 }
 
-fun first(): Int = withInput { input ->
-  0
+val part1Input = readInput().map {
+  val (d, m, _) = it.split(" ")
+  val dir = when (d) {
+    "R" -> East
+    "L" -> West
+    "U" -> North
+    "D" -> South
+    else -> throw IllegalArgumentException("Invalid direction")
+  }
+  m.toLong() to dir
 }
 
-fun second(): Int = withInput { input ->
-  0
+fun first(): Long = part1Input.getArea()
+
+val part2Input = readInput().map {
+  val (_, _, c) = it.split(" ")
+  val color = c.drop(1).dropLast(1)
+  val dir = when (color.last()) {
+    '0' -> East
+    '1' -> South
+    '2' -> West
+    '3' -> North
+    else -> throw IllegalArgumentException("Invalid direction")
+  }
+  color.drop(1).dropLast(1).toLong(radix = 16) to dir
+}
+
+
+fun second(): Long = part2Input.getArea()
+
+fun List<Pair<Long, Direction>>.getArea(): Long {
+  var start = 0L to 0L
+  val visited = mutableSetOf(start)
+  var points = 0L
+
+  forEach { (meters, dir) ->
+    start = start.first + dir.diff.first * meters to start.second + dir.diff.second * meters
+    points += meters
+    visited.add(start)
+  }
+
+  var area = 0L
+  val reversed = listOf(start) + visited.reversed()
+  reversed.zipWithNext().forEach { (p1, p2) ->
+    val (x1, y1) = p1
+    val (x2, y2) = p2
+    area += x1 * y2 - x2 * y1
+  }
+  return (area + points) / 2 + 1
 }
