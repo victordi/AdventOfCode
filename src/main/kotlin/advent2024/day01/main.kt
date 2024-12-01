88bc06986b5231e8c4e69b8d2939f690119e21bc
Victor Diaconu <diaconuvictor11@yahoo.com>
2024-12-01 07:11:03 +0200

    Day 1 2024

--- src/main/kotlin/advent2024/day01/main.kt
+++ src/main/kotlin/advent2024/day01/main.kt
@@ -2,16 +2,31 @@ package advent2024.day01
 
 import utils.Input.withInput
 import arrow.core.foldLeft
+import utils.splitTwo
+import kotlin.math.abs
 
 fun main() {
   println(first())
   println(second())
 }
 
-fun first(): Int = withInput { input ->
-  input.toList().size
+fun parseInput(): Pair<List<Int>, List<Int>> = withInput { input ->
+  val list = input.map { it.splitTwo("   ") { nr -> nr.toInt() } }.toList()
+  list.map { it.first }.sorted() to list.map { it.second }.sorted()
 }
 
-fun second(): Int = withInput { input ->
-  input.toList().size
+val sortedInput = parseInput()
+
+fun first(): Int = run {
+  val (first, second) = sortedInput
+  first.zip(second).sumOf {
+    abs(it.first - it.second)
+  }
+}
+
+fun second(): Int = run {
+  val (first, second) = sortedInput
+  first.sumOf { x ->
+    x * second.count { it == x }
+  }
 }
