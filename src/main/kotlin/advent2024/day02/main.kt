a675229f78f7f44acbcefd19ea0c9aa7f5ecc889
Victor Diaconu <diaconuvictor11@yahoo.com>
2024-12-02 07:26:26 +0200

    Day 2

--- src/main/kotlin/advent2024/day02/main.kt
+++ src/main/kotlin/advent2024/day02/main.kt
@@ -1,17 +1,34 @@
 package advent2024.day02
 
 import utils.Input.withInput
-import arrow.core.foldLeft
+import kotlin.math.abs
 
 fun main() {
   println(first())
   println(second())
 }
 
-fun first(): Int = withInput { input ->
-  input.toList().size
+fun checkReport(report: List<Int>): Boolean {
+  val zipped = report.zipWithNext()
+  val maxDiff = zipped.maxOf { abs(it.first - it.second) }
+  return maxDiff in 1..3 && (zipped.all { it.first < it.second } || zipped.all { it.first > it.second })
 }
 
+fun first(): Int = withInput { input ->
+  input.map { report ->
+    report.split(' ').map { it.toInt() }
+  }.count { checkReport(it) }
+}
+
+fun permutations(list: List<Int>): List<List<Int>> =
+  list.indices.map { index ->
+    list.filterIndexed { i, _ -> i != index }
+  }
+
 fun second(): Int = withInput { input ->
-  input.toList().size
+  input.map { report ->
+    report.split(' ').map { it.toInt() }
+  }
+    .map { permutations(it) }
+    .count { it.any { checkReport(it) } }
 }
