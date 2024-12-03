3b36872b9542a9d7eb4742931af18f15f76a711a
Victor Diaconu <diaconuvictor11@yahoo.com>
2024-12-03 07:20:44 +0200

    Day 3

--- src/main/kotlin/advent2024/day03/main.kt
+++ src/main/kotlin/advent2024/day03/main.kt
@@ -8,10 +8,32 @@ fun main() {
   println(second())
 }
 
-fun first(): Int = withInput { input ->
-  input.toList().size
+data class Instruction(val action: Int?, val a: Int, val b: Int)
+
+val regex = Regex("""mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)""")
+val instructions = regex.findAll(withInput { line -> line.toList().joinToString { it } })
+  .map {
+    val action = it.groupValues[0]
+    when (action) {
+      "do()" -> Instruction(1, 0, 0)
+      "don't()" -> Instruction(0, 0, 0)
+      else -> Instruction(null, it.groupValues[1].toInt(), it.groupValues[2].toInt())
+    }
+  }
+
+fun first(): Int = run {
+  instructions.sumOf { (_, a, b) -> a * b }
 }
 
-fun second(): Int = withInput { input ->
-  input.toList().size
+
+fun second(): Int = run {
+  instructions
+    .fold(1 to 0) { (enabled, sum), instruction ->
+      when (instruction.action) {
+        null -> enabled to sum + enabled * instruction.a * instruction.b
+        else -> instruction.action to sum
+      }
+    }
+    .second
+
 }
