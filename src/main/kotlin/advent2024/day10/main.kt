926ddf007e21e1fbbb47b0a191846a406aec6b89
Victor Diaconu <diaconuvictor11@yahoo.com>
2024-12-10 07:29:37 +0200

    Day 10

--- src/main/kotlin/advent2024/day10/main.kt
+++ src/main/kotlin/advent2024/day10/main.kt
@@ -1,16 +1,25 @@
 package advent2024.day10
 
-import utils.Input.withInput
+import utils.Direction
+import utils.Grid
+import utils.Point
+import utils.plus
 
 fun main() {
   println(first())
   println(second())
 }
 
-fun first(): Int = withInput { input ->
-  input.toList().size
-}
+val grid = Grid.fromInput { it.map { c -> c.digitToInt() } }
+val trailheads = grid.indexes.filter { grid[it] == 0 }.map { it.trailhead() }
 
-fun second(): Int = withInput { input ->
-  input.toList().size
-}
+fun Point.trailhead(): List<Point> =
+  if (grid[this] == 9) listOf(this)
+  else listOf(Direction.North, Direction.East, Direction.West, Direction.South).fold(emptyList()) { acc, direction ->
+    val next = grid.getOrNull(this + direction.diff) ?: -1
+    if (grid[this] + 1 == next) acc + (this + direction.diff).trailhead()
+    else acc
+  }
+
+fun first(): Int = trailheads.sumOf { it.toSet().size }
+fun second(): Int = trailheads.sumOf { it.size }
