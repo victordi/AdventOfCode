9aae0ce1ec38ba6b279ed27ea6438bfd248e8d17
Victor Diaconu <diaconuvictor11@yahoo.com>
2024-12-11 07:32:43 +0200

    Day 11

--- src/main/kotlin/advent2024/day11/main.kt
+++ src/main/kotlin/advent2024/day11/main.kt
@@ -1,16 +1,35 @@
 package advent2024.day11
 
+import arrow.core.foldLeft
 import utils.Input.withInput
 
 fun main() {
-  println(first())
-  println(second())
+  println(solve(25))
+  println(solve(75))
 }
 
-fun first(): Int = withInput { input ->
-  input.toList().size
+var stones = withInput { it.toList() }.first().split(" ").map { it.toLong() }
+
+fun blink(stone: Long): List<Long> =
+  if (stone == 0L) {
+    listOf(1L)
+  } else if (stone.toString().length % 2 == 0) {
+    val stoneString = stone.toString()
+    listOf(
+      stoneString.take(stoneString.length / 2).toLong(),
+      stoneString.takeLast(stoneString.length / 2).toLong()
+    )
+  } else {
+    listOf(stone * 2024L)
+  }
+
+fun Map<Long, Long>.blinkAll(): Map<Long, Long> = this.foldLeft(emptyMap()) { acc, (stone, count) ->
+  blink(stone).fold(acc) { acc2, newStone ->
+    acc2 + (newStone to (acc2[newStone] ?: 0) + count)
+  }
 }
 
-fun second(): Int = withInput { input ->
-  input.toList().size
+fun solve(count: Int): Long = run {
+  val initialStones = stones.groupingBy { it }.eachCount().mapValues { it.value.toLong() }
+  (1..count).fold(initialStones) { acc, _ -> acc.blinkAll() }.values.sum()
 }
