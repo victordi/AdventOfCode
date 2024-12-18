f223242d45f2a466cf98f0c916b696809ffa7500
Victor Diaconu <diaconuvictor11@yahoo.com>
2024-12-18 07:17:18 +0200

    Day 18

--- src/main/kotlin/advent2024/day18/main.kt
+++ src/main/kotlin/advent2024/day18/main.kt
@@ -1,5 +1,6 @@
 package advent2024.day18
 
+import utils.*
 import utils.Input.withInput
 
 fun main() {
@@ -7,10 +8,46 @@ fun main() {
   println(second())
 }
 
-fun first(): Int = withInput { input ->
-  input.toList().size
+val n = 71
+val bytes = withInput { input ->
+  input.toList().map { it.splitTwo(",").let { (a, b) -> a.toInt() to b.toInt() } }
+}
+val start = 0 to 0
+val end = n - 1 to n - 1
+val matrix = Array(n) { Array(n) { '.' } }
+
+fun Array<Array<Char>>.bfs(start: Point, end: Point): Int {
+  val visited = mutableSetOf<Point>()
+  val stack = mutableListOf(start to 0)
+
+  while (stack.isNotEmpty()) {
+    val (current, steps) = stack.removeAt(0)
+    if (current == end) return steps
+    if (current in visited) continue
+    visited.add(current)
+    current.getAdjacent()
+      .filter { (x, y) -> (x to y) !in visited && getOrElse(x to y, '#') != '#' }
+      .forEach { stack.add(it to steps + 1) }
+  }
+  return -1
 }
 
-fun second(): Int = withInput { input ->
-  input.toList().size
+fun first(): Int = run {
+  for ((x, y) in bytes.take(1024)) {
+    matrix[x][y] = '#'
+  }
+  matrix.bfs(start, end)
+}
+
+fun second(): Int = run {
+  var fallingByte = 1024
+  while(true) {
+    matrix[bytes[fallingByte]] = '#'
+    if (matrix.bfs(start, end) == -1) {
+      break
+    }
+    fallingByte++
+  }
+  println(bytes[fallingByte])
+  fallingByte
 }
