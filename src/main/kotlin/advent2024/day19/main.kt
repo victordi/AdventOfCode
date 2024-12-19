25c21b0f2cf6db9f672067447c18f4ef72e51c4a
Victor Diaconu <diaconuvictor11@yahoo.com>
2024-12-19 07:17:15 +0200

    Day 19

--- src/main/kotlin/advent2024/day19/main.kt
+++ src/main/kotlin/advent2024/day19/main.kt
@@ -7,10 +7,27 @@ fun main() {
   println(second())
 }
 
-fun first(): Int = withInput { input ->
-  input.toList().size
+val input = withInput { input ->
+  val list = input.toList()
+  val towels = list[0].split(",").map { it.trim() }
+  towels to list.drop(2)
 }
 
-fun second(): Int = withInput { input ->
-  input.toList().size
+val memo = mutableMapOf<String, Long>()
+fun String.countValid(towels: List<String>): Long =
+  if (this in memo) memo[this]!!
+  else if (isEmpty()) 1L
+  else towels.fold(0L) { acc, towel ->
+    acc + if (this.startsWith(towel)) drop(towel.length).countValid(towels) else 0
+  }.also { memo[this] = it }
+
+
+fun first(): Long = run {
+  val (towels, rules) = input
+  rules.sumOf { if (it.countValid(towels) != 0L) 1L else 0L }
+}
+
+fun second(): Long = run {
+  val (towels, rules) = input
+  rules.sumOf { it.countValid(towels) }
 }
