6d82e280897453b62e759701976c892240722f28
Victor Diaconu <diaconuvictor11@yahoo.com>
2025-12-02 10:12:25 +0200

    Day 2

--- src/main/kotlin/advent2025/day02/main.kt
+++ src/main/kotlin/advent2025/day02/main.kt
@@ -3,14 +3,17 @@ package advent2025.day02
 import utils.Input.withInput
 
 fun main() {
-  println(first())
-  println(second())
+  println(solve { isInvalid() })
+  println(solve { isInvalid2() })
 }
 
-fun first(): Int = withInput { input ->
-  input.toList().size
+fun solve(check: String.() -> Boolean): Long = withInput { input ->
+  input.first().split(",").sumOf { range ->
+    val (start, end) = range.split("-").map { it.toLong() }
+    (start..end).filter { it.toString().check() }.sum()
+  }
 }
 
-fun second(): Int = withInput { input ->
-  input.toList().size
-}
+fun String.isInvalid(): Boolean = substring(0, length / 2) == substring(length / 2)
+fun String.isInvalid2(): Boolean = (1..length / 2)
+  .any { patternLen -> chunked(patternLen).all { it == take(patternLen) } }
