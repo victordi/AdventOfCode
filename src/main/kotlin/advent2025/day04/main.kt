6f86a0fe1127a278832adbec1717d582b0d0ae1d
Victor Diaconu <diaconuvictor11@yahoo.com>
2025-12-04 07:34:18 +0200

    Day 4

--- src/main/kotlin/advent2025/day04/main.kt
+++ src/main/kotlin/advent2025/day04/main.kt
@@ -1,16 +1,37 @@
 package advent2025.day04
 
-import utils.Input.withInput
+import utils.Grid
+import utils.Point
 
 fun main() {
   println(first())
   println(second())
 }
 
-fun first(): Int = withInput { input ->
-  input.toList().size
+var map = Grid.fromInput()
+
+fun first(): Int = map.foldIndexed(0) { i, acc, _ ->
+  if (map[i] == '@' && map.getAdjacentWithCorners(i.first, i.second).count { it == '@' } < 4) {
+    acc + 1
+  } else {
+    acc
+  }
 }
 
-fun second(): Int = withInput { input ->
-  input.toList().size
+fun second(): Int = run {
+  var total = 0
+  var changed = true
+  while (changed) {
+    val cleared: List<Point> = map.foldIndexed(listOf()) { i, acc, _ ->
+      if (map[i] == '@' && map.getAdjacentWithCorners(i.first, i.second).count { it == '@' } < 4) {
+        acc + i
+      } else {
+        acc
+      }
+    }
+    changed = cleared.isNotEmpty()
+    total += cleared.size
+    map = map.mapIndexed { index, _ -> if (index in cleared) '.' else map[index] }
+  }
+  total
 }
