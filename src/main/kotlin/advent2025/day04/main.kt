d2ddcd5e7fcace6bf46210e9e8d2e8dc0e10779f
Victor Diaconu <diaconuvictor11@yahoo.com>
2025-12-04 07:53:47 +0200

    Day 4 optimized

--- src/main/kotlin/advent2025/day04/main.kt
+++ src/main/kotlin/advent2025/day04/main.kt
@@ -1,37 +1,23 @@
 package advent2025.day04
 
+import utils.Direction
 import utils.Grid
 import utils.Point
+import utils.move
 
 fun main() {
   println(first())
   println(second())
 }
 
-var map = Grid.fromInput()
+val paperRolls = Grid.fromInput().elements.filter { it.value == '@' }.keys
+fun Set<Point>.clear(): Set<Point> =
+  filter { point -> Direction.values().map { point.move(it) }.count { it in this } < 4 }.toSet()
 
-fun first(): Int = map.foldIndexed(0) { i, acc, _ ->
-  if (map[i] == '@' && map.getAdjacentWithCorners(i.first, i.second).count { it == '@' } < 4) {
-    acc + 1
-  } else {
-    acc
-  }
-}
+fun first(): Int = paperRolls.clear().size
 
-fun second(): Int = run {
-  var total = 0
-  var changed = true
-  while (changed) {
-    val cleared: List<Point> = map.foldIndexed(listOf()) { i, acc, _ ->
-      if (map[i] == '@' && map.getAdjacentWithCorners(i.first, i.second).count { it == '@' } < 4) {
-        acc + i
-      } else {
-        acc
-      }
-    }
-    changed = cleared.isNotEmpty()
-    total += cleared.size
-    map = map.mapIndexed { index, _ -> if (index in cleared) '.' else map[index] }
-  }
-  total
-}
+fun second(): Int = (1..Int.MAX_VALUE).fold(Triple(paperRolls, 0, true)) { (current, total, go), _ ->
+  if (!go) return total
+  val toClear = current.clear()
+  Triple(current - toClear, total + toClear.size, toClear.isNotEmpty())
+}.second
