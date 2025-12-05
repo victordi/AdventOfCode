306d2bb20ab8403269a12e31f65259d5bc15e509
Victor Diaconu <diaconuvictor11@yahoo.com>
2025-12-05 07:32:42 +0200

    Day 5

--- src/main/kotlin/advent2025/day05/main.kt
+++ src/main/kotlin/advent2025/day05/main.kt
@@ -1,16 +1,25 @@
 package advent2025.day05
 
-import utils.Input.withInput
+import utils.Input.readInput
+import utils.splitList
+import utils.splitTwo
 
 fun main() {
   println(first())
   println(second())
 }
 
-fun first(): Int = withInput { input ->
-  input.toList().size
-}
+val input = readInput().splitList { it.isEmpty() }
+val ranges = input.first().map { it.splitTwo("-") }.map { (a, b) -> a.toLong()..b.toLong() }
+val ids = input.drop(1).first().map { it.toLong() }
 
-fun second(): Int = withInput { input ->
-  input.toList().size
-}
+fun first(): Int = ids.filter { id -> ranges.any { id in it } }.size
+
+fun second(): Long = ranges
+  .sortedBy { it.first }.fold(0L to -1L) { (total, leftMost), range ->
+    val start = maxOf(range.first, leftMost + 1)
+    val inc = if (start <= range.last) {
+      range.last - start + 1
+    } else 0L
+    total + inc to maxOf(leftMost, range.last)
+  }.first
